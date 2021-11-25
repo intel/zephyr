f71191e495a249ad7df4aaf94a53e2fc46649d47
Nachiketa Kumar <nachiketa.kumar@intel.com>
2021-11-26 01:16:27 +0530

    pm: Add check for initialized devices during pm operations
    Adds check for initialized devices during pm operations
    
    Signed-off-by: Nachiketa Kumar <nachiketa.kumar@intel.com>

--- subsys/pm/device.c
+++ subsys/pm/device.c
@@ -32,6 +32,10 @@ static int _pm_devices(enum pm_device_state state)
 	for (const struct device *dev = devs + devc - 1; dev >= devs; dev--) {
 		int ret;
 
+		if (dev->state->init_res) {
+			continue;
+		}
+
 		/* ignore busy devices */
 		if (pm_device_is_busy(dev) || pm_device_wakeup_is_enabled(dev)) {
 			continue;
