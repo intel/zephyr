c558698b777839d3e2e23c4a88b14bbaa77af216
Jay Vasanth <jay.vasanth@microchip.com>
2022-04-19 18:16:07 -0400

    soc: microchip_mec: Prepare for MEC172x SAF version 2
    Microchip MEC172x eSPI SAF has significant hardware changes
    requiring a new SAF configuration structure. In preparation
    for the MEC172x we move the current SAF header out of common
    to the MEC1501 subfolder, remove an unused and empty common
    header and remove includes of the moved headers from MEC172x.
    
    Signed-off-by: Jay Vasanth <jay.vasanth@microchip.com>

--- soc/arm/microchip_mec/mec1501/soc.h
+++ soc/arm/microchip_mec/mec1501/soc.h
@@ -17,7 +17,7 @@
 #include "../common/soc_gpio.h"
 #include "../common/soc_pins.h"
 #include "../common/soc_espi_channels.h"
-#include "../common/soc_espi_saf.h"
+#include "soc_espi_saf_v1.h"
 
 #endif
 
